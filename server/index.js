01341c2324e35e904fbd33794be7aa31c5d362d1
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-13 16:22:49 -0700

    Sort by value whether ASC or DESC

--- server/index.js
+++ server/index.js
@@ -91,13 +91,14 @@ const sortPosts = (key, order) => function (a, b) {
     return 0;
   }
 
-  const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
-  const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+  const valA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+  const valB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
 
   let comparison = 0;
-  if (varA > varB) {
+
+  if (valA > valB) {
     comparison = 1;
-  } else if (varA < varB) {
+  } else if (valA < valB) {
     comparison = -1;
   }
   return (
