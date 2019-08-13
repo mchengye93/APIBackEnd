7601a75eed0c903bd74bd15d9a140f1a2d9679fa
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-13 16:21:14 -0700

    Sort posts by key

--- server/index.js
+++ server/index.js
@@ -91,10 +91,8 @@ const sortPosts = (key, order) => function (a, b) {
     return 0;
   }
 
-  const varA = (typeof a[key] === 'string')
-    ? a[key].toUpperCase() : a[key];
-  const varB = (typeof b[key] === 'string')
-    ? b[key].toUpperCase() : b[key];
+  const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+  const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
 
   let comparison = 0;
   if (varA > varB) {
